6a552f96ac2c5b812527208d4322206951e8794a
Roland Kullet <renaud.bellec.3@gmail.com>
2016-03-18 14:46:21 +0100

    removed useless article actions

--- conf.inc.php
+++ conf.inc.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 define("DBHOST","localhost");
 define("DBUSER","root");
 define("DBPWD","root");
-define("DBNAME","projet_annuel");
+define("DBNAME","ninja_db");
 
 DEFINE("SALT", "ABFffds32EOPDx32snklf443dsFDS464");
--- controllers/indexController.class.php
+++ controllers/indexController.class.php
@@ -1,15 +1,9 @@
-<?php 
+<?php
 
 class indexController
 {
 	public function indexAction($args)
 	{
-
-		$articles = new articles();
-		$articles->setTitle("Mon titre");
-		$articles->setContent("Description de mon article");
-		$articles->save();
-
 		$v = new view();
 		$v->setView("indexIndex");
 		$v->assign("pseudo","mec");
