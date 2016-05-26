9f260adc9c6f30463288a4259d88247c864ef1e9
Lambot Romain <lambot.rom@gmail.com>
2016-05-26 09:10:00 +0200

    Patching conlicts for PR

--- models/Team.class.php
+++ models/Team.class.php
@@ -1,15 +1,5 @@
 <?php
 
-
-class Team extends basesql
-{
-	protected $id;
-	protected $table = "teams";
-	protected $teamName;
-	protected $dateCreated;
-	protected $sports = <?php
-
-
 class Team extends basesql
 {
 	protected $id;
@@ -93,59 +83,3 @@ class Team extends basesql
 	}
 
 };
-	protected $description = "";
-	protected $img = "";
-
-	public function __construct(){
-		parent::__construct();
-	}
-
-	public function getId() {
-		return $this->id;
-	}
-
-	public function getTeamName(){
-		return $this->teamName;
-	}
-
-	public function getDateCreated() {
-		return $this->dateCreated;
-	}
-
-	public function getSports(){
-		return $this->sports;
-	}
-
-	public function getDescription() {
-		return $this->description;
-	}
-
-	public function getImg(){
-		return $this->img;
-	}
-
-	public function setId($id) {
-		$this->idTeam = $id;
-	}
-
-	public function setTeamName($teamName){
-		$this->teamName = $teamName;
-	}
-
-	public function setDateCreated($dateCreated) {
-		$this->dateCreated = $dateCreated;
-	}
-
-	public function setSports($sports){
-		$this->sports = $sports;
-	}
-
-	public function setDescription($description) {
-		$this->description = $description;
-	}
-
-	public function setImg($img){
-		$this->img = $img;
-	}
-
-}
