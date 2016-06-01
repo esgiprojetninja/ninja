7bbe92139eeac65b6c0d12cbb1b6bcdbb38e0f01
Roland Kullet <renaud.bellec.3@gmail.com>
2016-06-01 16:17:29 +0200

    dashboard

--- views/indexIndex.php
+++ views/indexIndex.php
@@ -1,1 +1,26 @@
-<h1><?php echo "Bienvenue" ?></h1>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="panel panel-primary2">
+            <div class="panel-heading">10 last subscribers</div>
+            <div class="panel-body">
+                <h1>COMING SOON</h1>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="panel panel-success">
+            <div class="panel-heading">10 last teams</div>
+            <div class="panel-body">
+                <h1>COMING SOON</h1>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="panel panel-warning">
+            <div class="panel-heading">10 last events</div>
+            <div class="panel-body">
+                <h1>COMING SOON</h1>
+            </div>
+        </div>
+    </div>
+</div>
