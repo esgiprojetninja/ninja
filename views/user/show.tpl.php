846a031f100a311183dee28658c8c7b67127cd3a
JacquesCeChirac <nicolas.cherridi@gmail.com>
2016-07-04 10:47:22 +0200

    First step: user/show

--- views/user/show.tpl.php
+++ views/user/show.tpl.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-12">
         <div class="panel panel-primary">
             <?php if(!$user->getUsername()):?>
             <div class="panel-body">
@@ -12,7 +12,7 @@
             <?php else : ?>
             
             
-            <div class="panel-heading"><h3 class="upper center"><?php echo $user->getUsername(); ?></h3></div>
+            <div class="panel-heading"><h3 class="upper center">Your profile</h3></div>
             
             <div class="panel-media">
                 <?php if($user->getAvatar() != ""): ?>
@@ -21,35 +21,48 @@
             </div>
 
             <div class="panel-body">
-                <ul>
-                    <li>
-                        <span class="fa fa-at"></span>
-                        <?php echo $user->getEmail(); ?>
+                <ul class="header-ul">
+                    <li class="header-li">Your informations :</li>
+                    <li class="li-list fa fa-user"></li>
+                    <li class="li-list">
+                        <span class="form-info">Your username :</span>
+                        <span class="form-content"><?php echo $user->getUsername(); ?></span>
                     </li>
-                    <li>
-                        <span class="fa fa-user"></span>
-                        <?php echo $user->getFirstName(); ?>
-                        <?php echo $user->getLastName(); ?>
+                    <li class="li-list">
+                        <span class="form-info">Your lastname :</span>
+                        <span class="form-content"><?php echo $user->getLastName(); ?></span>
                     </li>
-                    <li>
-                        <span class="fa fa-phone"></span>
-                        <?php echo $user->getPhoneNumber(); ?>
+                    <li class="li-list">
+                        <span class="form-info">Your firstname :</span>
+                        <span class="form-content"><?php echo $user->getFirstName(); ?></span>
                     </li>
-                    <?php if(!empty($teams)){ ?><li>
-                        <span class="fa fa-team"></span>
-                        <?php 
+                </ul>
+                <ul class="header-ul">
+                    <li class="li-list"><span class=" fa fa-phone"></span> <span class=" fa fa-at"></span></li>
+                    <li class="li-list">
+                        <span class="form-info">Email address :</span>
+                        <span class="form-content"><?php echo $user->getEmail(); ?></span>
+                    </li>
+                    <li class="li-list">
+                        <span class="form-info">Phone number :</span>
+                        <span class="form-content"><?php echo $user->getPhoneNumber(); ?></span>
+                    </li>
+                </ul>
+                <ul class="header-ul">
+                    <span class="fa fa-team header-li">Your groups :</span>
+                    <?php if(!empty($teams)){ ?>
+                        <?php
                             foreach($teams as $team){ 
                                 $Team = Team::findById($team[1]);
-                                echo '<a href='.WEBROOT.'team/show/'.$team[1].'>';
+                                echo '<li class="li-list"><a href='.WEBROOT.'team/show/'.$team[1].'>';
                                 echo $Team->getTeamName()."<br>";
-                                echo '</a>';
+                                echo '</li></a>';
                             }
                         ?>
-                    </li>
                     <?php } ?>
                 </ul>
                 <div class="text-right">
-                    <a href="<?= WEBROOT; ?>user/edit/<?php echo $user->getId(); ?>" class="btn btn-primary">Edit</a>
+                    <a href="<?= WEBROOT; ?>user/edit/<?php echo $user->getId(); ?>" class="btn btn-primary">Edit your profile</a>
                 </div>
             </div>
             <?php endif;?>
