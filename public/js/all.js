7abf7bdeafc9a4c59fe9aab798cecd4edc979847
JacquesCeChirac <nicolas.cherridi@gmail.com>
2016-07-11 17:49:13 +0200

    zob le responsive

--- public/js/all.js
+++ public/js/all.js
@@ -223,7 +223,7 @@ $(function ($) {
     $("#popinNotifications").append("<ul class='dropdown-menu notifications left' id='listeNotifications'>");
     $.getJSON( webrootJs+"notification/list", function(notifications) {
         var nbNotifications = 0;
-        $("#listeNotifications").append("<li class=\"notifications-heading\">Notifications</li></ul><div id=\"tata>\"><ul id='scroll'>");
+        $("#listeNotifications").append("<li class=\"notifications-heading global\">Notifications</li></ul><div ><ul id='scroll'>");
         for (var notification in notifications) {
             if (notifications[notification].opened == 1){
                 $("#scroll").append("<li id=\"notif\" class=\"notificationsLi opened\"><a href="+notifications[notification].action+" data-id=\"" + notifications[notification].id + "\">"+notifications[notification].message+"</a></li>");
