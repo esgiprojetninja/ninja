724265c35379037e342b61594d9370584d8425e7
JacquesCeChirac <nicolas.cherridi@gmail.com>
2016-07-24 17:23:24 +0200

    visuelcomment

--- views/event/comment.tpl.php
+++ views/event/comment.tpl.php
@@ -4,41 +4,50 @@
 
 <div class="row">
     <div class="col-sm-12">
-        <div class="panel panel-primary">
+        <div class="panel panel-primary2">
             <?php if(!$event->getId()):?>
             <div class="panel-body">
                 <h3>Event not found</h3>
             </div>
             <?php else : ?>
-
-
-            <div class="panel-heading"><h3 class="center header-li"> <?php echo $event->getName(); ?> event</h3></div>
-
-            <div class="panel-body">
-                <div class="col-sm-12">
+            <div class="panel-heading"><h3 class="center header-li"> <?php echo $event->getName(); ?> event comments</h3></div>
+        </div>
+        
                   <?php if(count($comments) == 0): ?>
                     <h2> There's no comments yet! </h2>
                   <?php elseif(count($comments) == 1):?>
                       <?php $user = User::findBy("id",$comments->getIdAuthor(),"int");?>
-                      <h2><?= $user->getUsername(); ?></h2> said <?= $comments->getIdComment(); ?> at <?= $comments->getDateCreated();?>
+                      <div class="panel panel-success">
+                          <div class="panel-heading">
+                              <a href="<?= WEBROOT ?>/user/show/<?= $user->getId(); ?>"><?= $user->getUsername(); ?></a>  said at <?= $comments->getDateCreated();?> :
+                          </div>
+                          <div class="panel-body">
+                              <p><?= $comments->getIdComment(); ?></p>
+                          </div>
+                      </div>
                   <?php else: ?>
+                      
                     <?php
                     foreach($comments as $comment){
                         $user = User::findBy("id",$comment->getIdAuthor(),"int");
-                      ?>
-                      <h3> <a href="<?= WEBROOT ?>/user/show/<?= $user->getId(); ?>"><?= $user->getUsername(); ?></a>  said at <?= $comment->getDateCreated();?> : </>
-                        <h2><?= $comment->getIdComment(); ?></h2>
+                    ?>
+                        <div class="panel panel-success">
+                            <div class="panel-heading">
+                                <a href="<?= WEBROOT ?>/user/show/<?= $user->getId(); ?>"><?= $user->getUsername(); ?></a>  said at <?= $comment->getDateCreated();?> :
+                            </div>
+                            <div class="panel-body">
+                                <p><?= $comment->getIdComment(); ?></p>
+                            </div>
+                        </div>
 
-                      <?php
+
+
+                    <?php
                     }
                     ?>
                   <?php endif; ?>
-                </div>
-            </div>
 
             <?php $this->createForm($commentForm, $commentErrors); ?>
-
-        </div>
       <?php endif; ?>
     </div>
 </div>
