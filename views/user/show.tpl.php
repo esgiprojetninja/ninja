65c241695840fdb89b441c618eed5e45e4e3342a
Roland Kullet <renaud.bellec.3@gmail.com>
2016-07-26 11:13:31 +0200

    quick fix + db

--- views/user/show.tpl.php
+++ views/user/show.tpl.php
@@ -62,6 +62,7 @@
                                 <span class="form-info">City : </span>
                                 <span class="form-content"><?php echo $user->getCity(); ?></span>
                             </li>
+                            <?php if ($user->getId() != $_SESSION["user_id"]): ?>
                             <li class="li-list">
                                 <div class="">
                                     <span class="form-info">Rate : </span>
@@ -72,6 +73,7 @@
                                     <button type="button" class="btn btn-primary js-user-vote" data-vote="down">-</button>
                                 </div>
                             </li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
