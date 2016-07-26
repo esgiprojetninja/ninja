a3c68e44ff8221339c1f96c1c1b04f44e41d16bb
Gauthi3r <mysterandco@gmail.com>
2016-07-26 12:24:08 +0200

    update new mail subscribe - mergeable !

--- views/subscribe_mail_html.php
+++ views/subscribe_mail_html.php
@@ -1,37 +1,49 @@
 
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
  
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8; image/gif" />
-	<title> SPORT NATION | WORLD WIDE </title>
+	<title> WELCOME | SPORT NATION | WORLD WIDE </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
-<body style="margin: 0; padding: 0;">
+<body style="margin: 0; padding: 0; font-family: sans-serif, Tahoma;">
 	<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
 		<tr>
-			<td align="center" bgcolor="#8BC34A" style="padding: 40px 0 30px 0;">
-				<a href="<?= User::getEmailLink("subscribe"); ?>">
-					<img src="<?= WEBROOT; ?>public/img/email/logo.gif" alt="Email Activation World Nation" width="300" height="230" style="display: block;" />
-				</a>
-				<p style="padding: 40px 0 0 40px"> FOR ACTIVATE YOUR ACCOUNT CLIC ON LOGO </p>
+			<td align="center" bgcolor="#6C6599" style="padding: 40px 40 30px 0; border-bottom: 2px solid #888;"> 
+				<h3 style="color: #fff;"> Welcome </h2> 
 			</td>
 		</tr>
 		<tr>
-			<td bgcolor="#eee" style="padding: 40px 30px 40px 30px;">
+			<td align="center" bgcolor="#fff" style="padding: 40px 0 40px 0;">
+				
+					<img src="<?= WEBROOT; ?>public/img/email/logo.gif" alt="Profil activation" width="300" height="230" style="display: block; margin-bottom: 40px;" >
+					<p style="font-weight: 700; margin-bottom: 40px;"> To confirm your account, click the button, please</p>
+				<a style="padding: 15px; border: 1px solid #555; background-color: #328396; border-radius: 10px; color: #fff; text-decoration: none; font-weight: 600" href="<?= User::getEmailLink("subscribe"); ?>""> Confirm your profil </a>
+			</td>
+		</tr>
+		<tr>
+			<td bgcolor="#fff" style="padding: 20px 30px 40px 20px; border-top: 2px solid #888;">
 				<table border="0" cellpadding="0" cellspacing="0" width="100%">
+					<tr>
+						<h3 style="color: #000; text-decoration: underline"> Our Flux Rss </h3>
+
+					</tr>
 					<tr>
 						<td width="260" valign="top">
 							<table border="0" cellpadding="0" cellspacing="0" width="100%">
 								<tr>
 									<td>
-										<img src="<?= WEBROOT; ?>public/img/email/volleyball.gif" alt="" width="100%" height="140" style="display: block;" />
+										<img src="<?= WEBROOT; ?>public/img/email/volleyball.gif" alt="" width="90%" height="140" style="display: block; border-radius: 0px; border: 3px solid #555;" />
 									</td>
 								</tr>
 								<tr>
-									<td style="padding: 25px 0 0 50px;"> TEAM LISTE </td>
+									<td style="padding: 25px 0 0 50px;"> 
+										<a style="padding: 15px; border-bottom: 2px solid #555; background-color: #C4770A; border-radius: 2px; color: #fff; text-decoration: none; font-weight: 600" href="<?= WEBROOT; ?>/feed/team"> Flux Team </a>
+									</td>
 								</tr>
 							</table>
 						</td>
@@ -40,11 +52,11 @@
 							<table border="0" cellpadding="0" cellspacing="0" width="100%">
 							<tr>
 								<td>
-									<img src="<?= WEBROOT; ?>public/img/email/footing.gif" alt="" width="100%" height="140" style="display: block;" />
+									<img src="<?= WEBROOT; ?>public/img/email/volleyball.gif" alt="" width="90%" height="140" style="display: block; border-radius: 15px; border: 3px solid #555;" />
 								</td>
 							</tr>
 								<tr>
-									<td style="padding: 25px 0 0 50px;"> EVENT LISTE </td>
+									<td style="padding: 25px 0 0 50px;"> <a style="padding: 0px; border-bottom: 2px solid #555; background-color: #C4770A; border-radius: 2px; color: #fff; text-decoration: none; font-weight: 600" href=href="<?= WEBROOT; ?>/feed/event"> Flux Event </a> </td>
 								</tr>
 							</table>
 						</td>
@@ -56,6 +68,11 @@
 			<td bgcolor="#444" style="padding: 30px 30px 30px 30px;">
 				<table border="0" cellpadding="0" cellspacing="0" width="100%">
 					<tr>
+						<td align="left">
+    					    <p style="color: #fff;">
+          						Copyrigth Sports Nation© | All rights reserved
+        					</p>
+						</td>
 						<td align="right">
 							<table border="0" cellpadding="0" cellspacing="0">
 								<tr>
